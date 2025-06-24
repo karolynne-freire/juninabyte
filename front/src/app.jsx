ce331a7dd1e3e6e84d6553c9cc160ac683355fda
karolynne-freire <freirekarolynne@gmail.com>
2025-06-23 22:27:32 -0300

    adicionar barra de progresso com animação de milho acompanhando o preenchimento

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -6,12 +6,32 @@ import { Header } from './components/Header/Header';
 
 
 export function App() {
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
+    
     <>
+
+{loading ? (
+        <div class="loading-screen">
+        <div class="barra-container">
+          <div class="barra-preenchida">
+            <span class="milho">🌽</span>
+          </div>
+        </div>
+      </div>
+      ) : (
+        <div className="app-content">
       <Navbar />
       <Bandeirinhas />
       <Header />
       <Footer />
+        </div>
+      )}
     </>
   );
 }
